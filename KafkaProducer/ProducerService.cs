aa43f037bc022a102568e6250ed76eb115682230
Sajith Dilhan <sajithdilhan@gmail.com>
2022-12-11 11:34:54 +0800

    Add project files.

---
+++ KafkaProducer/ProducerService.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaProducer
+{
+    internal class ProducerService
+    {
+        public ProducerService() { }
+
+        private readonly ProducerConfig config = new ProducerConfig
+        { 
+            BootstrapServers = "localhost:9092" 
+        };
+
+        private readonly string topic = "kafkaproducer.samplemessage";
+        public void SendMessage(string message)
+        {
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    producer.ProduceAsync(topic, new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Something went wrong: {e}");
+                }
+            }
+        }
+
+
+    }
+}
+
+
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using KafkaProducer;
+
+string input = string.Empty;
+var producer = new ProducerService();
+do
+{
+    Console.WriteLine("Enter message");
+    input = Console.ReadLine() ?? string.Empty;
+    producer.SendMessage(input);
+}
+while (!input.Equals("Exit", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Exiting....");
+Environment.Exit(0);
